d467255273d38eb55d0038c2ed68093cafc18a8b
Chris Sophiea <csophiea@gmail.com>
2021-02-22 14:03:21 -0700

    added token to httpclient requests on create game
    Also changed statlineid on games to be nullable

--- Repository/Migrations/20210222205239_m3.Designer.cs
+++ Repository/Migrations/20210222205239_m3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repository;
 
 namespace Repository.Migrations
 {
     [DbContext(typeof(SeasonContext))]
-    partial class SeasonContextModelSnapshot : ModelSnapshot
+    [Migration("20210222205239_m3")]
+    partial class m3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
